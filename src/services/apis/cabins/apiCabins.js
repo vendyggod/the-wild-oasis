b3806a5ce4ad6c6bb4a8fe22fffcd92fd6ea5556
vendyggod <charli98@mail.ru>
2024-12-08 22:55:40 +0300

    fix: api file structure. feat: create new cabins api functions

---
+++ src/services/apis/cabins/apiCabins.js
@@ -0,0 +1,30 @@
+import supabase from '../../supabase';
+
+export async function getCabins() {
+  const { data, error } = await supabase.from('cabins').select('*');
+
+  if (error) {
+    console.error(error);
+    throw new Error('Cabins could not be loaded');
+  }
+
+  return data;
+}
+
+export async function createCabin(newCabin) {
+  const { error } = await supabase.from('cabins').insert([newCabin]);
+
+  if (error) {
+    console.error(error);
+    throw new Error('Cabin could not be created');
+  }
+}
+
+export async function deleteCabin(id) {
+  const { error } = await supabase.from('cabins').delete().eq('id', id);
+
+  if (error) {
+    console.error(error);
+    throw new Error('Cabin could not be deleted');
+  }
+}
