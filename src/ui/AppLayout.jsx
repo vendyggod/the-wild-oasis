bb0be31a6136c4a84e3600cb878bf82b41190f74
vendyggod <charli98@mail.ru>
2024-12-08 22:58:47 +0300

    fix: create a container over Outlet component

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -14,13 +14,23 @@ const StyledAppLayout = styled.div`
   height: 100dvh;
 `;
 
+const Container = styled.div`
+  max-width: 120rem;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  gap: 3.2rem;
+`;
+
 function AppLayout() {
   return (
     <StyledAppLayout>
       <Header />
       <Sidebar />
       <Main>
-        <Outlet />
+        <Container>
+          <Outlet />
+        </Container>
       </Main>
     </StyledAppLayout>
   );
