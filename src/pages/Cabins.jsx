a2d10f8af8b81e27fbfaa2e450b8fc4926be1bc8
vendyggod <charli98@mail.ru>
2024-12-08 22:52:52 +0300

    fix: imports and component tree

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,11 +1,25 @@
-import { Heading, Row } from '../ui';
+import { useState } from 'react';
+import CabinTable from '../features/cabins/CabinTable';
+import { Button, Heading, Row } from '../ui';
+import CreateCabinForm from '../features/cabins/CreateCabinForm';
 
 function Cabins() {
+  const [showForm, setShowForm] = useState(false);
+
   return (
-    <Row type="horizontal">
-      <Heading as="h1">All cabins</Heading>
-      <p>TEST</p>
-    </Row>
+    <>
+      <Row type="horizontal">
+        <Heading as="h1">All cabins</Heading>
+        <p>TEST</p>
+      </Row>
+      <Row>
+        <CabinTable />
+        <Button onClick={() => setShowForm((show) => !show)}>
+          Add new cabin
+        </Button>
+        {showForm && <CreateCabinForm />}
+      </Row>
+    </>
   );
 }
 
