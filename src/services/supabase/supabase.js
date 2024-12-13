9a30cce30ae30e75913f4c475020262bfb7cc6e0
vendyggod <charli98@mail.ru>
2024-12-13 22:28:18 +0300

    chore: export supabaseUrl

--- src/services/supabase/supabase.js
+++ src/services/supabase/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://gykadskozmoaxslaggqn.supabase.co';
+export const supabaseUrl = 'https://gykadskozmoaxslaggqn.supabase.co';
 const supabaseKey =
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd5a2Fkc2tvem1vYXhzbGFnZ3FuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzMwODE3NzAsImV4cCI6MjA0ODY1Nzc3MH0.tYbODayfwxmuBuqjkZ8zET4wTx-9dqL_ttohqGzCaXg';
 const supabase = createClient(supabaseUrl, supabaseKey);
