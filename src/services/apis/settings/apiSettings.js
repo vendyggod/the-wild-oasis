3dfaf325fdcb5f05311eb733ac5ab4434693dcbe
vendyggod <charli98@mail.ru>
2024-12-17 22:35:17 +0300

    chore: delete console.logs

--- src/services/apis/settings/apiSettings.js
+++ src/services/apis/settings/apiSettings.js
@@ -4,7 +4,6 @@ export async function getSettings() {
   const { data, error } = await supabase.from('settings').select('*').single();
 
   if (error) {
-    console.error(error);
     throw new Error('Settings could not be loaded');
   }
   return data;
@@ -20,8 +19,7 @@ export async function updateSetting(newSetting) {
     .single();
 
   if (error) {
-    console.error(error);
-    throw new Error('Settings could not be updated');
+    throw new Error('Setting could not be updated');
   }
   return data;
 }
